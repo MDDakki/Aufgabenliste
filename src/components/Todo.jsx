02370d8d2dbf188a0caff6297ebfbb8d33cac0e5
MoDakki <mohammaddakki@gmail.com>
2024-01-28 21:58:51 +0100

    React Icons & Hinzufügen einer Aufgabe & Kommentar

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
 
-function Todo() {
-  return (
-    <div>Todo</div>
-  )
+function Todo({ completeTodo, todos }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo__row complete" : "todo__row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiCloseCircleLine />
+        <TiEdit />
+      </div>
+    </div>
+  ));
 }
 
-export default Todo 
+export default Todo;
--- src/components/TodoListe.jsx
+++ src/components/TodoListe.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo'
 
 function TodoListe() {
  const [todos, setTodos] = useState([])
+
+
  const addTodo = todo => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
         return
@@ -13,10 +16,21 @@ function TodoListe() {
     console.log(...todos)
  }
 
+ const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.isComplete = !todo.isComplete
+        } 
+        return todo
+    })
+    setTodos(updatedTodos)
+ }
+
   return (
     <div>
         <h1>Hallo</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo}></Todo>
     </div>
   )
 }
